96bb42829e5e357a3b8b45e94e70577597859b76
Florian ALU <florian.alu@Digital-Grenoble.local>
2021-03-10 10:18:25 +0100

    Add: for loop to render multiple suppliers

--- src/components/SuppliersList.vue
+++ src/components/SuppliersList.vue
@@ -2,6 +2,8 @@
     <div>
         <h1>Liste des fournisseurs</h1>
         <Supplier
+            v-for="{ id, name, status, checkedAt } of suppliers"
+            v-bind:key="id"
             :name="name"
             :status="status"
             :checked-at="checkedAt"
@@ -17,9 +19,20 @@ export default {
     components: {Supplier},
     data() {
         return {
-            name: 'Mon fournisseur',
-            status: false,
-            checkedAt: new Date
+            suppliers: [
+                {
+                    id: 1,
+                    name: "Fournisseur 1 ?",
+                    status: true,
+                    checkedAt: new Date()
+                },
+                {
+                    id: 2,
+                    name: "Fournisseur 2",
+                    status: false,
+                    checkedAt: new Date()
+                }
+            ]
         }
     }
 }
